ae018d6741031debe8bd6dfa51c9d167415dfe8f
Matheus Rodrigues Guedes <matheusguedes25@gmail.com>
2022-09-12 12:34:43 -0300

    Security - authenticação em memória

---
+++ src/main/java/com/zappts/magicthegathering/api/config/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.zappts.magicthegathering.api.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/*
+ * EnableWebSecurity --> desabilita as configurações padrões do security e habilita so as confg personalizadas;
+ * 
+ * httpBasic() --> padrão do spring security
+ * and() 	   --> returna um builder para unir as customizações
+ * 
+ * Proteção contra ameças:
+ * CSRF --> https://docs.spring.io/spring-security/reference/features/exploits/csrf.html
+ * */
+
+//@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// config de autenticação proprio secutity
+		http.httpBasic()
+			.and()
+			.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and().csrf().disable();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// Config de autenticação em memória
+		auth.inMemoryAuthentication()
+			.withUser("matheus")
+			.password(passwordEncoder().encode("12345"))
+			.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
