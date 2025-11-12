442871a8b3594d2f9b418e504bdd0283d6c3e575
MatheusRGuedes <matheusguedes25@gmail.com>
2025-11-12 16:55:42 -0300

    Jogador Test / MapStruct

--- src/main/java/com/zappts/magicthegathering/api/dto/JogadorDTO.java
+++ src/main/java/com/zappts/magicthegathering/api/dto/JogadorDTO.java
@@ -9,37 +9,45 @@ import io.swagger.annotations.ApiModel;
 @ApiModel(value = "Jogador - Request")
 public class JogadorDTO {
 
-	private Long id;
-	
-	@Length(max = 60)
-	@NotBlank
-	private String username;
-	
-	@NotBlank
-	private String password;
-	
-	public JogadorDTO() {
-		
-	}
-		
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+    private Long id;
 
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    @Length(max = 60)
+    @NotBlank
+    private String username;
 
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    @NotBlank
+    private String password;
+
+    public JogadorDTO() {
+
+    }
+
+    public JogadorDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/zappts/magicthegathering/domain/service/JogadorService.java
+++ src/main/java/com/zappts/magicthegathering/domain/service/JogadorService.java
@@ -10,7 +10,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.zappts.magicthegathering.core.exceptions.BusinesException.Codes;
 import com.zappts.magicthegathering.core.exceptions.JogadorNotFoundException;
 import com.zappts.magicthegathering.core.exceptions.UsernameExistsException;
 import com.zappts.magicthegathering.domain.model.Jogador;
@@ -19,61 +18,60 @@ import com.zappts.magicthegathering.domain.repository.JogadorRepository;
 @Service
 public class JogadorService implements UserDetailsService {
 
-	@Autowired
-	private JogadorRepository repository;
-	
-	private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-	
-	
-	public List<Jogador> getAll() {
-		return repository.findAll();
-	}
-	
-	public Jogador findOne(Long id) {
-		
-		if (!repository.existsById(id)) {
-			throw new JogadorNotFoundException(id);
-		}
-		
-		Optional<Jogador> jogador = repository.findById(id);
-		return jogador.get();
-	}
-	
-	public Jogador create(Jogador jogador) {
-		
-		if (repository.findByUsername(jogador.getUsername()).isPresent()) {
-			throw new UsernameExistsException();
-		}
-		jogador.setPassword(passwordEncoder.encode(jogador.getPassword()));
-		return repository.save(jogador);
-	}
-	
-	public Jogador update(Long id, Jogador request) {
-		
-		if (!repository.existsById(id)) {
-			throw new JogadorNotFoundException(id);
-		}
-		
-		request.setId(id);
-		
-		return repository.save(request);
-	}
-	
-	public List<Jogador> delete(Long id) {
-		
-		if (!repository.existsById(id)) {
-			throw new JogadorNotFoundException(id);
-		}
-		
-		repository.deleteById(id);
-		
-		return getAll();
-	}
-	
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Jogador jogador = repository.findByUsername(username)
-				.orElseThrow(() -> new UsernameNotFoundException("User not found for username: "+ username));
-		return jogador;
-	}
+    @Autowired
+    private JogadorRepository repository;
+
+    private static final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public List<Jogador> getAll() {
+        return repository.findAll();
+    }
+
+    public Jogador findOne(Long id) {
+
+        if (!repository.existsById(id)) {
+            throw new JogadorNotFoundException(id);
+        }
+
+        Optional<Jogador> jogador = repository.findById(id);
+        return jogador.get();
+    }
+
+    public Jogador create(Jogador jogador) {
+
+        if (repository.findByUsername(jogador.getUsername()).isPresent()) {
+            throw new UsernameExistsException();
+        }
+        jogador.setPassword(passwordEncoder.encode(jogador.getPassword()));
+        return repository.save(jogador);
+    }
+
+    public Jogador update(Long id, Jogador request) {
+
+        if (!repository.existsById(id)) {
+            throw new JogadorNotFoundException(id);
+        }
+
+        request.setId(id);
+
+        return repository.save(request);
+    }
+
+    public List<Jogador> delete(Long id) {
+
+        if (!repository.existsById(id)) {
+            throw new JogadorNotFoundException(id);
+        }
+
+        repository.deleteById(id);
+
+        return getAll();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Jogador jogador = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found for username: " + username));
+        return jogador;
+    }
 }
